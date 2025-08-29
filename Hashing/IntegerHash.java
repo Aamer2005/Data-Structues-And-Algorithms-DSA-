a924460a0bc39b700b564611abebf115d76727cd
Aamer2005 <2023bit064@sggs.ac.in>
2025-08-29 17:16:32 +0530

    Integer Hashing Concepts

---
+++ Hashing/IntegerHash.java
@@ -0,0 +1,32 @@
+class Hash{
+
+int[] Hashnumber = new int[1000001];
+
+
+public void HashingFunctionNumber(int[] array)
+{
+    for(int i=0;i<array.length;i++){
+            Hashnumber[array[i]] +=1;
+        }
+}
+
+
+
+public int getHashValue(int key ){
+
+        return Hashnumber[key];
+}
+
+public static void main(String[] args)
+{
+    Hash h = new Hash();
+    int[] array = {1,2,3,4,5,6,7,8,9,1,2,3,4,5};
+    h.HashingFunctionNumber(array);
+
+    System.out.println(h.getHashValue(1));
+    System.out.println(h.getHashValue(4));
+    System.out.println(h.getHashValue(7));
+
+}
+
+}
