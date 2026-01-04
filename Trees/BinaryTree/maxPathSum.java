9626ce256c964efd9cc73e0a1c268a2b156a6aea
Aamer2005 <2023bit064@sggs.ac.in>
2026-01-04 08:03:29 +0530

    main

---
+++ Trees/BinaryTree/maxPathSum.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxPathSum(TreeNode root) {
+
+        int[] maxvalue = {(int)Integer.MIN_VALUE};
+
+        maxPathDown(root,maxvalue);
+
+        return maxvalue[0];
+    }
+    public int maxPathDown(TreeNode root , int[] maxvalue)
+    {
+        if(root==null)return 0;
+
+        int left = Math.max(0,maxPathDown(root.left,maxvalue));
+        int right = Math.max(0,maxPathDown(root.right,maxvalue));
+        maxvalue[0] = Math.max(maxvalue[0],left+right+root.val);
+
+        return Math.max(left,right)+root.val;
+    }
+}
