598527e8e6ebbd6d231bd83de59c126cd013ec7a
Aamer2005 <2023bit064@sggs.ac.in>
2026-04-01 15:49:30 +0530

    tabulation In Dynamic programming

---
+++ Dynamic_Programming/tabulation.java
@@ -0,0 +1,19 @@
+class tabulation{
+
+public static int fibo(int n)
+{
+
+    int prev1 = 1 , prev2 = 0 , current=0;
+
+    for(int i=2;i<=n;i++)
+    {
+        current = prev1+prev2;
+        prev2=prev1;
+        prev1 = current;
+    }
+
+    return current;
+}
+
+
+}
