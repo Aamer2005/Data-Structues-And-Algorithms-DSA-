f0ac0ca1a4a54d6baa7d650459442fa1eeb677c2
Aamer2005 <2023bit064@sggs.ac.in>
2026-04-06 13:37:06 +0530

    0/1 knapsack with memoization

--- Dynamic_Programming/knapsack.java
+++ Dynamic_Programming/knapsack.java
@@ -1,8 +1,48 @@
+//knapsack using memoization
+class KnapSack{
+
+    public static int knapsack(int W,int[] val , int[] wt , int n ,int[][] memo){
+        if(W==0 || n<0) return 0;
+
+        if(memo[n][W]!=-1){
+            return memo[n][W];
+
+        }
+
+        int pick = 0;
+
+        if(wt[n]<=W)
+        pick = val[n]+knapsack(W-wt[n],val,wt,n-1,memo);
+
+        int notpick = knapsack(W,val,wt,n-1,memo);
+
+        return memo[n][W] = Math.max(pick,notpick);
+    }
+
+    public static void main(String[] args){
+        int n = 3;
+       int  W = 4;
+       int val[] = {1,2,3};
+       int wt[] = {4, 5, 1};
+       int[][] memo = new int[n + 1][W + 1];
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <= W; j++)
+                memo[i][j] = -1;
+        }
+
+       System.out.println(knapsack(W,val,wt , n-1,memo));
+
+    }
+
+}
+
+
 //knapsack using recursion only
 class KnapSackRecursion{
 
     public static int knapsack(int W,int[] val , int[] wt , int n){
-        if(W==0 || n==0) return 0;
+        if(W==0 || n<0) return 0;
 
         int pick = 0;
 
