0d6ff8f8b34ec7ad78c6e2a14d09e78f62b12cc0
Aamer2005 <2023bit064@sggs.ac.in>
2026-04-06 13:59:34 +0530

    ...

--- Arrays/CheckDuplicateInArray.java
+++ Arrays/CheckDuplicateInArray.java
@@ -17,6 +17,29 @@ class Solution {
 
         return false;
     }
+
+    public boolean containsDuplicateUsingArray(int[] nums){
+
+        int[] arr = new int[11];//assumes all elements inserted are less than equal 10
+
+        for(int i=0;i<nums.length;i++){
+            arr[nums[i]]++;
+        }
+
+        java.util.ArrayList<Integer> al = new java.util.ArrayList<>();
+        boolean result = false;
+        for(int i = 0;i<arr.length;i++)
+        {
+            if(arr[i]>1)
+            {
+            result = true;
+            al.add(i);
+            }
+        }
+        System.out.println(al);
+        return true;
+
+    }
 }
 
 //O(n) complexity using hash
