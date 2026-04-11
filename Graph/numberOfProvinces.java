19aa23bdad6927c935d6d63efbaeb1539706393a
Aamer2005 <2023bit064@sggs.ac.in>
2026-04-11 16:25:40 +0530

    optimal solution with )(n^n) to find number of provinces

--- Graph/numberOfProvinces.java
+++ Graph/numberOfProvinces.java
@@ -1,4 +1,47 @@
 //leetcode 547 : Number Of Provinces
+
+//1ms of time
+class Solution {
+    public int findCircleNum(int[][] isConnected) {
+
+        return DFS(isConnected.length , isConnected);
+        
+    }
+
+    int DFS( int V , int[][] adj)
+    {
+        int count = 0;
+        boolean[] visited = new boolean[V];
+
+        for(int i=0;i<V;i++)
+        {
+            if(!visited[i])
+            {
+                count+=1;
+                visit_DFS( i , adj,visited);
+            }
+        }
+
+        return count;
+    }
+
+    void visit_DFS(int v , int[][] adj,boolean[] visited)
+    {
+        visited[v] = true;
+
+        for(int j=0;j<adj.length;j++)
+        {
+            int x = adj[v][j]; //check j is neighbor to v or not if x==1 then neighbor
+            if(x==1 && !visited[j] && v!=j) //
+            {
+                visit_DFS(j,adj,visited);
+            }
+        }
+    }
+}
+
+
+//3ms of time
 class Solution {
     public int findCircleNum(int[][] isConnected) {
 
