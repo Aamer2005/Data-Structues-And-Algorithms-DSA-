2679071f8a77a2398723c503244b02a7eaa0abb1
Aamer2005 <2023bit064@sggs.ac.in>
2026-04-12 11:26:45 +0530

    Find number of islands

---
+++ Graph/numberOfIslands.java
@@ -0,0 +1,39 @@
+//200. Number of Islands
+class Solution {
+    public int numIslands(char[][] grid) {
+
+        int count = 0;
+
+        for(int i=0;i<grid.length;i++)
+        {
+            for(int j=0;j<grid[0].length;j++)
+            {
+                if(grid[i][j]=='1')
+                {
+                    count+=1;
+                    dfs_visit(i,j,grid);
+                }
+            }
+        }
+
+        return count ;
+    }
+
+    void dfs_visit(int i , int j , char[][] grid)
+    {
+        grid[i][j] = '0';
+
+        if(i-1>=0 && grid[i-1][j]=='1')
+        dfs_visit(i-1,j,grid);
+
+        if(j-1>=0 && grid[i][j-1]=='1')
+        dfs_visit(i,j-1,grid);
+
+        if(j+1<grid[0].length && grid[i][j+1]=='1')
+        dfs_visit(i,j+1,grid);
+
+        if(i+1<grid.length && grid[i+1][j]=='1')
+        dfs_visit(i+1,j,grid);
+
+    }
+}
